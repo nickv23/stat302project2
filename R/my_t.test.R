e436dc06c4efdfae30cfcf14b4c455e1d3396b2b
nav23 <nicholasverghese23@gmail.com>
2020-12-16 14:13:50 -0800

    Add documentation to my_t.test file

--- R/my_t.test.R
+++ R/my_t.test.R
@@ -1,9 +1,30 @@
+#' Hypothesis Test Function
+#'
+#' This function runs a one sample t test
+#'
+#' @param x Numeric vector of data
+#' @param alternative Character that represents what the alternative hypothesis is
+#' @param mu Numeric that represents the null hypothesis value for the mean
+#' @keywords inference
+#'
+#' @return A list with the following variables:
+#' \itemize {
+#' \item test_stat - A numeric representing the t value
+#' \item df - A numeric representing the degrees of freedom
+#' \item alternative - A character that represents what the alternative hypothesis is
+#' \item p_val - A numeric representing the p value
+#' }
+#'
+#' @examples
+#' my_t.test(my_gapminder$lifeExp, "greater", 59)
+#'
+#' @export
 my_t.test <- function(x, alternative, mu) {
-  #Calculate degrees of freedom and t values
+  # Calculate degrees of freedom and t values
   df <- length(x) - 1
   t_val <- (mean(x) - mu) / (sd(x) / sqrt(length(x)))
 
-  #Determine the alt hypothesis to calculate correct p value, throw error if not valid alt hypothesis
+  # Determine the alt hypothesis to calculate correct p value, throw error if not valid alt hypothesis
   if (alternative == "less") {
     p_val <- pt(t_val, df, lower.tail = TRUE)
   } else if (alternative == "greater") {
@@ -14,7 +35,7 @@ my_t.test <- function(x, alternative, mu) {
     stop("Please enter in a valid alternative hypothesis: 'greater', 'less', or 'two.sided'")
   }
 
-  #Return list of necessary values
+  # Return list of necessary values
   answers <- list("test_stat" = t_val, "df" = df, "alternative" = alternative, "p_val" = p_val)
   return(answers)
 }
